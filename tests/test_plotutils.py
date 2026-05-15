0aa1fc8ae42c3f2f713b43854c6eb2228f5e20ed
Corey R. Randall <corey.r.randall@gmail.com>
2026-05-15 16:49:24 -0600

    Refactor plotutils tests into class-based organization

--- tests/test_plotutils.py
+++ tests/test_plotutils.py
@@ -13,169 +13,162 @@ from ampworks.plotutils._plotly import _render_plotly
 
 
 # tests for _colors submodule
-def test_cmap_init():
+class TestColorMap:
 
-    cm = aplt.ColorMap('viridis', (0, 1))
+    def test_init(self):
+        cm = aplt.ColorMap('viridis', (0, 1))
 
-    assert cm._vmin == 0
-    assert cm._vmax == 1
-    assert hasattr(cm, '_sm')
+        assert cm._vmin == 0
+        assert cm._vmax == 1
+        assert hasattr(cm, '_sm')
 
-    with pytest.raises(ValueError):
-        aplt.ColorMap('viridis', (0,))  # norm must be length 2
+        with pytest.raises(ValueError):
+            aplt.ColorMap('viridis', (0,))  # norm must be length 2
 
-    with pytest.raises(ValueError):
-        aplt.ColorMap('viridis', (1, 0))  # vmin must be < vmax
+        with pytest.raises(ValueError):
+            aplt.ColorMap('viridis', (1, 0))  # vmin must be < vmax
 
+    def test_get_color(self):
+        cm = aplt.ColorMap('viridis', (0, 1))
+        color = cm.get_color(0.5)
 
-def test_cmap_get_color():
+        assert isinstance(color, tuple)
+        assert len(color) == 4  # RGBA
 
-    cm = aplt.ColorMap('viridis', (0, 1))
+        with pytest.raises(ValueError):
+            cm.get_color(1.5)
 
-    color = cm.get_color(0.5)
+    def test_colors_from_size(self):
+        size = 5
+        colors = aplt.colors_from_size(size, 'viridis')
 
-    assert isinstance(color, tuple)
-    assert len(color) == 4  # RGBA
+        assert isinstance(colors, list)
+        assert len(colors) == size
+        assert all(len(c) == 4 for c in colors)
 
-    cm = aplt.ColorMap('viridis', (0, 1))
-    with pytest.raises(ValueError):
-        cm.get_color(1.5)
+    def test_colors_from_data(self):
+        data = np.array([[0, 0.5], [0.8, 1]])
+        colors = aplt.colors_from_data(data, 'viridis')
 
+        assert isinstance(colors, np.ndarray)
+        assert colors.shape == data.shape
 
-def test_cmap_colors_from_size():
-
-    size = 5
-    colors = aplt.colors_from_size(size, 'viridis')
-
-    assert isinstance(colors, list)
-    assert len(colors) == size
-    assert all(len(c) == 4 for c in colors)
-
-
-def test_cmap_colors_from_data():
-
-    data = np.array([[0, 0.5], [0.8, 1]])
-    colors = aplt.colors_from_data(data, 'viridis')
-
-    assert isinstance(colors, np.ndarray)
-    assert colors.shape == data.shape
-
-    for row in colors:
-        for c in row:
-            assert len(c) == 4
+        for row in colors:
+            for c in row:
+                assert len(c) == 4
 
 
 # tests for _text submodule
-def test_add_text():
+class TestAddText:
 
-    # basic
-    fig, ax = plt.subplots()
+    def test_add_text(self):
+        fig, ax = plt.subplots()
 
-    aplt.add_text(ax, 0.1, 0.1, 'First')
-    aplt.add_text(ax, 0.9, 0.9, 'Second')
+        aplt.add_text(ax, 0.1, 0.1, 'First')
+        aplt.add_text(ax, 0.9, 0.9, 'Second')
 
-    texts = [t.get_text() for t in ax.texts]
+        texts = [t.get_text() for t in ax.texts]
 
-    assert 'First' in texts
-    assert 'Second' in texts
-    assert len(ax.texts) == 2
+        assert 'First' in texts
+        assert 'Second' in texts
+        assert len(ax.texts) == 2
 
-    plt.close(fig)
+        plt.close(fig)
 
-    # alignment
-    fig, ax = plt.subplots()
+    def test_add_text_alignment(self):
+        fig, ax = plt.subplots()
 
-    aplt.add_text(ax, 0.3, 0.7, 'Aligned', ha='left', va='top')
+        aplt.add_text(ax, 0.3, 0.7, 'Aligned', ha='left', va='top')
 
-    text = ax.texts[0]
+        text = ax.texts[0]
 
-    assert text.get_ha() == 'left'
-    assert text.get_va() == 'top'
+        assert text.get_ha() == 'left'
+        assert text.get_va() == 'top'
 
-    plt.close(fig)
+        plt.close(fig)
 
 
 # tests for _ticks submodule
-def test_minor_ticks():
+class TestTicks:
 
-    # defaults
-    fig, ax = plt.subplots()
-    aplt.minor_ticks(ax)
+    def test_minor_ticks_defaults(self):
+        fig, ax = plt.subplots()
+        aplt.minor_ticks(ax)
 
-    assert isinstance(ax.xaxis.get_minor_locator(), AutoMinorLocator)
-    assert isinstance(ax.yaxis.get_minor_locator(), AutoMinorLocator)
+        assert isinstance(ax.xaxis.get_minor_locator(), AutoMinorLocator)
+        assert isinstance(ax.yaxis.get_minor_locator(), AutoMinorLocator)
 
-    plt.close(fig)
+        plt.close(fig)
 
-    # custom
-    fig, ax = plt.subplots()
-    aplt.minor_ticks(ax, xdiv=4, ydiv=3)
+    def test_minor_ticks_custom(self):
+        fig, ax = plt.subplots()
+        aplt.minor_ticks(ax, xdiv=4, ydiv=3)
 
-    xloc = ax.xaxis.get_minor_locator()
-    yloc = ax.yaxis.get_minor_locator()
+        xloc = ax.xaxis.get_minor_locator()
+        yloc = ax.yaxis.get_minor_locator()
 
-    assert isinstance(xloc, AutoMinorLocator) and xloc.ndivs == 4
-    assert isinstance(yloc, AutoMinorLocator) and yloc.ndivs == 3
+        assert isinstance(xloc, AutoMinorLocator) and xloc.ndivs == 4
+        assert isinstance(yloc, AutoMinorLocator) and yloc.ndivs == 3
 
-    plt.close(fig)
+        plt.close(fig)
 
+    def test_tick_direction_defaults(self):
+        fig, ax = plt.subplots()
+        aplt.tick_direction(ax)
 
-def test_tick_direction():
+        xparams = ax.xaxis.get_tick_params()
+        yparams = ax.yaxis.get_tick_params()
 
-    # defaults
-    fig, ax = plt.subplots()
-    aplt.tick_direction(ax)
+        if 'top' not in xparams.keys():  # for backwards mpl, v <= 3.9
+            xparams['top'] = xparams.get('right')
 
-    xparams = ax.xaxis.get_tick_params()
-    yparams = ax.yaxis.get_tick_params()
+        assert xparams['direction'] == 'in' and xparams['top']
+        assert yparams['direction'] == 'in' and yparams['right']
 
-    if 'top' not in xparams.keys():  # for backwards mpl, v <= 3.9
-        xparams['top'] = xparams.get('right')
+        plt.close(fig)
 
-    assert xparams['direction'] == 'in' and xparams['top']
-    assert yparams['direction'] == 'in' and yparams['right']
+    def test_tick_direction_custom(self):
+        fig, ax = plt.subplots()
+        aplt.tick_direction(
+            ax, xdir='out', ydir='inout', top=False, right=False,
+        )
 
-    plt.close(fig)
+        xparams = ax.xaxis.get_tick_params()
+        yparams = ax.yaxis.get_tick_params()
 
-    # custom
-    fig, ax = plt.subplots()
-    aplt.tick_direction(ax, xdir='out', ydir='inout', top=False, right=False)
+        if 'top' not in xparams.keys():  # for backwards mpl, v <= 3.9
+            xparams['top'] = xparams.get('right')
 
-    xparams = ax.xaxis.get_tick_params()
-    yparams = ax.yaxis.get_tick_params()
+        assert xparams['direction'] == 'out' and not xparams['top']
+        assert yparams['direction'] == 'inout' and not yparams['right']
 
-    if 'top' not in xparams.keys():  # for backwards mpl, v <= 3.9
-        xparams['top'] = xparams.get('right')
+        plt.close(fig)
 
-    assert xparams['direction'] == 'out' and not xparams['top']
-    assert yparams['direction'] == 'inout' and not yparams['right']
+    def test_format_ticks(self):
+        fig, ax = plt.subplots()
+        aplt.format_ticks(
+            ax,
+            xdiv=4, ydiv=3,
+            xdir='out', ydir='inout',
+            top=False, right=False,
+        )
 
-    plt.close(fig)
+        xloc = ax.xaxis.get_minor_locator()
+        yloc = ax.yaxis.get_minor_locator()
 
+        xparams = ax.xaxis.get_tick_params()
+        yparams = ax.yaxis.get_tick_params()
 
-def test_format_ticks():
+        assert isinstance(xloc, AutoMinorLocator) and xloc.ndivs == 4
+        assert isinstance(yloc, AutoMinorLocator) and yloc.ndivs == 3
 
-    fig, ax = plt.subplots()
-    aplt.format_ticks(
-        ax, xdiv=4, ydiv=3, xdir='out', ydir='inout', top=False, right=False,
-    )
+        if 'top' not in xparams.keys():  # for backwards mpl, v <= 3.9
+            xparams['top'] = xparams.get('right')
 
-    xloc = ax.xaxis.get_minor_locator()
-    yloc = ax.yaxis.get_minor_locator()
+        assert xparams['direction'] == 'out' and not xparams['top']
+        assert yparams['direction'] == 'inout' and not yparams['right']
 
-    xparams = ax.xaxis.get_tick_params()
-    yparams = ax.yaxis.get_tick_params()
-
-    assert isinstance(xloc, AutoMinorLocator) and xloc.ndivs == 4
-    assert isinstance(yloc, AutoMinorLocator) and yloc.ndivs == 3
-
-    if 'top' not in xparams.keys():  # for backwards mpl, v <= 3.9
-        xparams['top'] = xparams.get('right')
-
-    assert xparams['direction'] == 'out' and not xparams['top']
-    assert yparams['direction'] == 'inout' and not yparams['right']
-
-    plt.close(fig)
+        plt.close(fig)
 
 
 # tests for plotutils._plotly._render_plotly

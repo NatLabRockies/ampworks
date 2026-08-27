20866b154354129efa2c820e9273b8da3f2aaf5c
Corey R. Randall <corey.r.randall@gmail.com>
2026-08-27 09:45:11 -0600

    Add _std_head module with standard column name constants
    Port the _std_head module from inprogress/_core into src/ampworks/_core, providing SECONDS, AMPS, VOLTS, CYCLE, STEP, STATE, AH, WH, DATETIME, and WATTS constants for use as argument defaults across the package.

---
+++ src/ampworks/_core/_std_head.py
@@ -0,0 +1,25 @@
+"""Standard names for common data columns."""
+
+__all__ = [
+    'SECONDS',
+    'AMPS',
+    'VOLTS',
+    'CYCLE',
+    'STEP',
+    'STATE',
+    'AH',
+    'WH',
+    'DATETIME',
+    'WATTS',
+]
+
+SECONDS = 'Seconds'
+AMPS = 'Amps'
+VOLTS = 'Volts'
+CYCLE = 'Cycle'
+STEP = 'Step'
+STATE = 'State'
+AH = 'Ah'
+WH = 'Wh'
+DATETIME = 'DateTime'
+WATTS = 'Watts'
